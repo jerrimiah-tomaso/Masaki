31db312d54234ac447e222d2d40629d9a5a9bf37
Jerrimiah <idogamedevfrfr@gmail.com>
2026-05-14 23:01:54 -0500

    Update main.js

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,1 +1,12 @@
-console.log("Assistant loaded!");
+console.log("MASAKI V1 loaded!");
+
+const backendStatus = document.getElementById("status");
+
+fetch("http://127.0.0.1:8000/")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.message === "active") {
+      backendStatus.innerText = "Backend: Active";
+    }
+    console.log(data.message);
+  });
