16ef52b0f6965453c7ab2376a282b07191a2402a
Jerrimiah <idogamedevfrfr@gmail.com>
2026-05-14 23:01:31 -0500

    Added parser.py

---
+++ backend/parser.py
@@ -0,0 +1,21 @@
+numbers = 1234567890
+
+valid_commands = ["timer","alarm"]
+
+def parse(data):
+
+    commands = []
+
+    tokens = data.split()
+
+    print("Parsing: ", data)
+
+    for token in tokens:
+        if token in valid_commands:
+            current_command = token
+        elif token.isdigit() and current_command is not None:
+            commands.append((current_command, int(token)))
+            current_command = None
+    
+    print(commands)
+
